837bb7cfe49c988faabd46cf6c1c6e2333df64c3
Peter S. Loft <peterloft3@gmail.com>
2021-04-22 10:49:57 -0500

    Update Bet.sol
    Updating basic bet contract to reflect advancements in understanding of the contract ecosystem.

--- Contracts/Bet.sol
+++ Contracts/Bet.sol
@@ -3,57 +3,106 @@ pragma solidity >=0.5.0 <0.6.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/ownership/Ownable.sol";
 
+
+
 // @title This contract is the basic Bet creation tool
 // @author Dirgeguy
 // @notice This contract is intended to track, make, and payout bets created by users. 
-// @dev Currently, the deployment blockchain has not been chosen, so the contract will contain basic 
-//  infrastructure to support Loom, ETH, or other solidity accepting chains.
+// @dev This file will contain the bet factory used to as a portal to transcribe user bets from the front-end to the
+// blockchain itself. This way, every bet will be secured and trackable on-chain. Still need to implement several aspects,
+// specifically the tracking of new/open bet contracts, and then find a reasonably easy way to delete them from our front-end
+// and free up space on the chain, if possible. 
 
 contract BetMaker is Ownable {
     
     using SafeMath for uint256;
     
     // This event is to be used as an alert to front-end new bet has been created and to make it viewable 
-    event BetMade(address issuer, address taker, uint betId, uint bag, uint8 sport, uint8 winCondition);
-    
+    event BetMade(address issuer, address taker, uint betId, uint bag, uint16 sport, uint16 winCondition);
     
     mapping (uint => address) public betToOwner;
     mapping (address => uint) makerBetCount;
     
-    struct Bet {
+    address payable banker;
+    address _bAddress;
+    
+    struct BetArray {
         address issuer;
         address taker;
         uint bag;
-        uint8 sport;
-        uint8 winCondition;
+        uint16 sport;
+        uint16 winCondition;
     } 
     
-    Bet[] public bets;
+    BetArray[] public bets;
     
     // @dev This function's main intention is to accept parameters from a front-end interface and lock all conditions onto
     //      the chain. This function will create a separate contract holding a payload (value of total bet, i.e. _bag) and
     //      and have functionality to then, upon the completion of the bet (_winCondition is met or failed) the bag (minus 
-    //      the fee levied by us) is distributed to the winner. 
-    function _Bet(address _issuer, address _taker, uint _bag, uint8 _sport, uint8 _winCondition) public {
-        
-        
+    //      the fee levied by us) is distributed to the winner.
+    
+    function _commitBet(address payable _issuer, address payable _taker) public onlyOwner returns (address) {
+       Bet _b = new Bet(_issuer, _taker, banker);
+       _bAddress = address(_b);
+       return _bAddress;
+    }
+    
+    function newBet() public view returns (address) {
+        return _bAddress;
+    }
+    
+    function _betMade(address _issuer, address _taker, uint _bag, uint16 _sport, uint16 _winCondition) internal {
         // This is all the information we would need to create a full record of the bet transaction.
-        uint id = bets.push(Bet(_issuer, _taker, _bag, _sport, _winCondition)) - 1;
+        uint id = bets.push(BetArray(_issuer, _taker, _bag, _sport, _winCondition)) - 1;
         betToOwner[id] = msg.sender;
         makerBetCount[msg.sender] = makerBetCount[msg.sender].add(1);
         emit BetMade(_issuer, _taker, id, _bag, _sport, _winCondition);
     }
     
-    // @dev Goal is to utilize an oracle to determine which side receives the bag at the end of the betting event. May need
-    //      may need to start with simple centralized oracle and API reporting before eventually building out to completely 
-    //      accept Chainlink or other truly decentralized Oracles.
-    function _whoWon(uint _betId) internal {
-        
+    function getContractCount() public view returns (uint contractCount) {
+        return bets.length;
     }
     
-    // This function will be used to query the Oracles and verify the outcome of the sport and winCondition to determine 
-    // who receives the bag 
-    function _payout() internal {
-        
+    function _setbank(address payable _bank) public onlyOwner {
+        banker = _bank;
     }
+
+}
+
+
+contract Bet {
+    
+    using SafeMath for uint256;
+    
+    address payable _issuer;
+    address payable _taker;
+    address payable _banker;
+    
+    constructor(
+        address payable issuer,
+        address payable taker,
+        address payable banker
+        ) 
+        public {
+            _issuer = issuer;
+            _taker = taker;
+            _banker = banker;
+    }
+    
+    function _bet(uint _bag /*, uint16 _sport, uint16 _winCondition*/) external payable {
+        require(msg.value == _bag);
+        // _betMade( _issuer, _taker, _bag, _sport, _winCondition);
+    }
+    
+    function winnerWinner(bool _issuerWin) external {
+        require(_banker == msg.sender, "Caller is not authorized for this function");
+            if(_issuerWin == true) {
+                _issuer.transfer(address(this).balance.sub(address(this).balance.div(100)));
+                _banker.transfer(address(this).balance);
+            } else {
+                _taker.transfer(address(this).balance.sub(address(this).balance.div(100)));
+                _banker.transfer(address(this).balance);
+            }
+    }
+    
 }
