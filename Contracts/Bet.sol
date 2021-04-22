9714f7837ec358e58526921f3e2f2fd19789d359
Peter S. Loft <peterloft3@gmail.com>
2021-04-21 20:45:51 -0500

    Update Bet.sol
    This branch will be used to experiment with contract factory and proxy solutions for the betting contracts.

--- Contracts/Bet.sol
+++ Contracts/Bet.sol
@@ -1,59 +1,58 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity 0.5.17;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/math/SafeMath.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/ownership/Ownable.sol";
+contract Counter {
 
-// @title This contract is the basic Bet creation tool
-// @author Dirgeguy
-// @notice This contract is intended to track, make, and payout bets created by users. 
-// @dev Currently, the deployment blockchain has not been chosen, so the contract will contain basic 
-//  infrastructure to support Loom, ETH, or other solidity accepting chains.
+    uint256 private _count;
+    address private _owner;
+    address private _factory;
 
-contract BetMaker is Ownable {
-    
-    using SafeMath for uint256;
-    
-    // This event is to be used as an alert to front-end new bet has been created and to make it viewable 
-    event BetMade(address issuer, address taker, uint betId, uint bag, uint8 sport, uint8 winCondition);
-    
-    
-    mapping (uint => address) public betToOwner;
-    mapping (address => uint) makerBetCount;
-    
-    struct Bet {
-        address issuer;
-        address taker;
-        uint bag;
-        uint8 sport;
-        uint8 winCondition;
-    } 
-    
-    Bet[] public bets;
-    
-    // @dev This function's main intention is to accept parameters from a front-end interface and lock all conditions onto
-    //      the chain. This function will create a separate contract holding a payload (value of total bet, i.e. _bag) and
-    //      and have functionality to then, upon the completion of the bet (_winCondition is met or failed) the bag (minus 
-    //      the fee levied by us) is distributed to the winner. 
-    function _Bet(address _issuer, address _taker, uint _bag, uint8 _sport, uint8 _winCondition) public {
-        
-        
-        // This is all the information we would need to create a full record of the bet transaction.
-        uint id = bets.push(Bet(_issuer, _taker, _bag, _sport, _winCondition)) - 1;
-        betToOwner[id] = msg.sender;
-        makerBetCount[msg.sender] = makerBetCount[msg.sender].add(1);
-        emit BetMade(_issuer, _taker, id, _bag, _sport, _winCondition);
+
+     modifier onlyOwner(address caller) {
+        require(caller == _owner, "You're not the owner of the contract");
+        _;
     }
-    
-    // @dev Goal is to utilize an oracle to determine which side receives the bag at the end of the betting event. May need
-    //      may need to start with simple centralized oracle and API reporting before eventually building out to completely 
-    //      accept Chainlink or other truly decentralized Oracles.
-    function _whoWon(uint _betId) internal {
-        
+
+    modifier onlyFactory() {
+        require(msg.sender == _factory, "You need to use the factory");
+        _;
     }
-    
-    // This function will be used to query the Oracles and verify the outcome of the sport and winCondition to determine 
-    // who receives the bag 
-    function _payout() internal {
-        
+
+     constructor(address owner) public {
+        _owner = owner;
+        _factory = msg.sender;
     }
+
+     function getCount() public view returns (uint256) {
+        return _count;
+    }
+
+    function increment(address caller) public onlyFactory onlyOwner(caller) {
+        _count++;
+    }
+
+}
+
+contract CounterFactory {
+
+    mapping(address => Counter) _counters;
+
+    function createCounter() public {
+        require (_counters[msg.sender] == Counter(0));
+        _counters[msg.sender] = new Counter(msg.sender);
+    }
+
+    function increment() public {
+        require (_counters[msg.sender] != Counter(0));
+        Counter(_counters[msg.sender]).increment(msg.sender);
+    }
+
+    function getCount(address account) public view returns (uint256) {
+        require (_counters[account] != Counter(0));
+        return (_counters[account].getCount());
+    }
+
+    function getMyCount() public view returns (uint256) {
+        return (getCount(msg.sender));
+    }
+
 }
