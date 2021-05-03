dd788d49cf504951e3e06269bcd7f1eab0cb86ff
Peter S. Loft <peterloft3@gmail.com>
2021-05-03 12:52:49 -0500

    Update OracleInterface.sol
    Updating all verbiage to better equate to other contracts and their respective comments/jargon.

--- sprts_bets/betting_side/contracts/OracleInterface.sol
+++ sprts_bets/betting_side/contracts/OracleInterface.sol
@@ -2,20 +2,20 @@ pragma solidity >=0.5.0;
 
 contract OracleInterface {
 
-    enum MatchOutcome {
-        Pending,    //match has not been fought to decision
-        Underway,   //match has started & is underway
+    enum GameOutcome {
+        Pending,    //game has not been fought to decision
+        Underway,   //game has started & is underway
         Draw,       //anything other than a clear winner (e.g. cancelled)
         Decided     //index of participant who is the winner 
     }
 
-    function getPendingMatches() public view returns (bytes32[] memory);
+    function getPendingGames() public view returns (bytes32[] memory);
 
-    function getAllMatches() public view returns (bytes32[] memory);
+    function getAllGames() public view returns (bytes32[] memory);
 
-    function matchExists(bytes32 _matchId) public view returns (bool); 
+    function gameExists(bytes32 _matchId) public view returns (bool); 
 
-    function getMatch(bytes32 _matchId) public view returns (
+    function getGame(bytes32 _matchId) public view returns (
         bytes32 id,
         string memory name,
         uint8 visitor,
@@ -23,10 +23,10 @@ contract OracleInterface {
         uint8 winner,
         uint8 line,
         uint date, 
-        MatchOutcome outcome
+        GameOutcome outcome
         );
 
-    function getMostRecentMatch(bool _pending) public view returns (
+    function getMostRecentGame(bool _pending) public view returns (
         bytes32 id,
         string memory name,
         uint8 visitor,
@@ -34,7 +34,7 @@ contract OracleInterface {
         uint8 winner,
         uint8 line,
         uint date, 
-        MatchOutcome outcome
+        GameOutcome outcome
         );
 
     function testConnection() public pure returns (bool);
